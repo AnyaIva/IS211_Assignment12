12713efb19af066e7dd5b0f08420a950f68923f8
AnyaIva <90570973+AnyaIva@users.noreply.github.com>
2022-12-05 14:10:25 -0500

    Create schema.sql

---
+++ schema.sql
@@ -0,0 +1,22 @@
+DROP TABLE IF EXISTS students;
+DROP TABLE IF EXISTS quizzes;
+DROP TABLE IF EXISTS quiz_results;
+
+CREATE TABLE students (
+   id INTEGER PRIMARY KEY,
+   first_name TEXT NOT NULL,
+   last_name TEXT NOT NULL
+);
+
+CREATE TABLE quizzes (
+   id INTEGER PRIMARY KEY NOT NULL,
+   subject TEXT NOT NULL,
+   questions INTEGER,
+   date TEXT NOT NULL
+);
+
+CREATE TABLE quiz_results (
+   student_id INT NOT NULL,
+   quizzes_id INT NOT NULL,
+   score INT NOT NULL
+);
